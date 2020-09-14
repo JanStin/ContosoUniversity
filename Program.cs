0ef76bdc7d595fdf3204e017315409ab1f78d462
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-09-14 22:58:04 +0300

    Change program.cs for create db. Created db

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ContosoUniversity.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ContosoUniversity
 {
@@ -13,7 +15,33 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        // Создаются БД на основе моделей, которые указаны в data/schoolContext. 
+        // Или удалет таблицы и создает заново, если модель данных измениться.
+        // Если БД существует, то ничего не происходит.
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
