47eb8959ba9bbdb8032b81d807ae4668dec271f0
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-09-16 21:08:53 +0300

    Added pagination in students page

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using ContosoUniversity.Support;
 
 namespace ContosoUniversity.Pages.Students
 {
@@ -25,13 +26,25 @@ namespace ContosoUniversity.Pages.Students
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public PaginatedList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
             MidNameSort = sortOrder == "MidName" ? "midName_desc" : "MidName";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             CurrentFilter = searchString;
 
@@ -44,29 +57,19 @@ namespace ContosoUniversity.Pages.Students
                                        || s.FirstMidName.Contains(searchString));
             }
 
-            switch (sortOrder)
+            studentsIQ = sortOrder switch
             {
-                case "name_desc":
-                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
-                    break;
-                case "Date":
-                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
-                    break;
-                case "date_desc":
-                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
-                    break;
-                case "MidName":
-                    studentsIQ = studentsIQ.OrderBy(s => s.FirstMidName);
-                    break;
-                case "midName_desc":
-                    studentsIQ = studentsIQ.OrderByDescending(s => s.FirstMidName);
-                    break;
-                default:
-                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
-                    break;
-            }
+                "name_desc" => studentsIQ.OrderByDescending(s => s.LastName),
+                "Date" => studentsIQ.OrderBy(s => s.EnrollmentDate),
+                "date_desc" => studentsIQ.OrderByDescending(s => s.EnrollmentDate),
+                "MidName" => studentsIQ.OrderBy(s => s.FirstMidName),
+                "midName_desc" => studentsIQ.OrderByDescending(s => s.FirstMidName),
+                _ => studentsIQ.OrderBy(s => s.LastName),
+            };
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            int pageSize = 3;
+            Students = await PaginatedList<Student>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
